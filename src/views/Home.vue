07118842b0e427ee8133d2b58d5eb22435ad67ff
chafau <chairilfauzi212@gmail.com>
2022-04-28 15:59:33 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main id="Home-page">
+    <Header />
+
+    <h1>Home</h1>
+    <p>This is the home page</p>
+  </main>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
